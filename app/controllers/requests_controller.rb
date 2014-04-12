7ce6dc2a0ebd5833110980ef1ff3b7622ba18f32
Sean Freiburg <frycicle@gmail.com>
2014-04-12 01:30:01 -0500

    echo

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,9 +5,9 @@ class RequestsController < ApplicationController
 
     @client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     @client.account.messages.create(
-        :from => '+12173344307',
-        :to => '+12176173798',
-        :body => 'Hey there!'
+        :from => @params["To"],
+        :to => @params["From"],
+        :body => @params["Body"]
     )
   end
 end
