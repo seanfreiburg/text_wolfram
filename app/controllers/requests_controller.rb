ba2d2d428675ebf65ab0632585317444742d2b7b
Sean Freiburg <frycicle@gmail.com>
2014-04-12 01:25:06 -0500

    fix for foregy

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,8 @@
 class RequestsController < ApplicationController
 
   def index
+    puts "Recieved request"
+
     @client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     @client.account.messages.create(
         :from => '+12173344307',
